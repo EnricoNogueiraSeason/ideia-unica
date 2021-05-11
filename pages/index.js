56d07d982850344a02a4e9dc654ff6372a38b118
Enrico Nogueira <enrico.nogueira@season.com.br>
2021-05-11 15:01:27 -0300

    finalizaçao do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
 
-function Contador(props) {
-    const [contador, setContador] = useState(1);
-
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
+        </div>)
 }
 
 export default Home
